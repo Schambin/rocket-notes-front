d8d7784fe099c6693aaf37b5745c00518a46f8a2
Kauan <raidoka546@gmail.com>
2024-05-31 10:51:07 -0400

    moved style to global archive

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import theme from './styles/theme'; 
+import GlobalStyles from './styles/global';
+import { ThemeProvider } from 'styled-components';
 import { Details } from './pages/Details/index.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Details />
+    <ThemeProvider theme={theme} > 
+      <GlobalStyles/>
+      <Details />
+    </ThemeProvider>
   </React.StrictMode>,
 )
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,13 +1,8 @@
 import styled from "styled-components";
 //Styled Css
-
 export const Container = styled.div`
-    background: red;
-    margin: 0;
-    padding: 0;
 `;
-
-
+// ----------------------------------------------------------------
 //Normal css
 // body {
 //     background: rgba(255, 52, 52, 0.624);
