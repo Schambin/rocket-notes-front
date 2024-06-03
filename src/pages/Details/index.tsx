cf958adb5627f5930c7e4d1a908486e46d5f221b
Kauan <raidoka546@gmail.com>
2024-06-03 15:20:18 -0400

    section n style to links

--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,14 +1,27 @@
-import { Container } from "./styles.js";
+import { Container, Links } from "./styles.js";
+
 import { Button } from "../../components/button"
 import { Header } from "../../components/header"
+import { Section } from "../../components/section"
 
 export function Details() {
 
   return(
     <Container>
+
       <Header />
-      
+
+      <Section title="Links Úteis"> 
+        <Links>
+          <li><a href="#"> Sexo </a></li>
+          <li>Sexo 2</li>
+        </Links>
+      </Section>
+        
+      <Section title="Marcadores"> 
+
+      </Section>
       <Button title="Voltar"/>
     </Container>
-  ) 
+  )
 }
