90e0d2d78eb9cd65b9225d22431110953fb63c06
Kauan <raidoka546@gmail.com>
2024-06-03 11:31:08 -0400

    header comp.

---
+++ src/components/header/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from './styles.js'
+
+export function Header() {
+    return (
+        <Container>
+            <h1>Rocket Notes</h1>
+            <span>Bem Vindo, Sacudo.</span>
+        </Container>
+    )
+}
