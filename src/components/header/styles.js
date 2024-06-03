dd8b5f8236e3e5759b1e12c11d45153facf3bcc0
Kauan <raidoka546@gmail.com>
2024-06-03 11:46:45 -0400

    img and message styles

--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -13,6 +13,32 @@ export const Container = styled.header`
     display: flex;
     justify-content: space-between;
     padding: 0 8rem;
+`;
 
-    background-color: red;
+export const Profile = styled.div`
+    display: flex;
+    align-items: center;
+
+    > img {
+        height: 5.6rem;
+        width: 5.6rem;
+        border-radius: 50%;
+    }
+
+    > div {
+        display: flex;
+        flex-direction: column;
+        margin-left: 1.6rem;
+        line-height: 2.4rem;
+
+        span {
+            font-size: 1.4rem;
+            color: ${({ theme }) => theme.COLORS.GRAY_100};
+        }
+
+        strong {
+            font-size: 1.8rem;
+            color: ${({ theme }) => theme.COLORS.WHITE};
+        }
+    }
 `;
