4392f33420979f2de9d7c66e51fd3ed9660de610
Kauan <raidoka546@gmail.com>
2024-06-18 10:33:05 -0400

    SIgnIn Page

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,15 @@ import theme from './styles/theme';
 import GlobalStyles from './styles/global';
 import { ThemeProvider } from 'styled-components';
 
-import { Home } from './pages/Home';
+// import { Home } from './pages/Home';
+import { SignIn } from './pages/SignIn';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme} > 
       <GlobalStyles/>
-      <Home />
+      {/* <Home /> */}
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
