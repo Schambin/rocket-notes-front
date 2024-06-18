79cdadbb3b5681fe1dc292d2d6f982734fb9958f
Kauan <raidoka546@gmail.com>
2024-06-18 10:33:14 -0400

    Icon to Button

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,8 +1,9 @@
 import { Container } from "./styles";
 
-export function Button({ title, Loading = false, ...rest }) {
+export function Button({ icon: Icon, title, Loading = false, ...rest }) {
     return (
         <Container type="button" disabled={Loading} {...rest} >
+             {Icon && <Icon size={10}/>}
             { Loading ? "Carregando..." : title }
         </Container>
     )
