56f14ec2cfd8209c3c4f7d3aeda41245ff7b8ef5
Kauan <raidoka546@gmail.com>
2024-06-20 16:11:21 -0400

    bg image and color for signin page

--- src/components/note/index.jsx
+++ src/components/note/index.jsx
@@ -5,14 +5,14 @@ export function Note({ data, ...rest }) {
     return (
         <Container { ...rest }>
             <h1>{data.title}</h1>
-            {
-                data.tags && 
-                <footer>
-                    {
-                        data.tags.map(tag => <Tag key={tag.name} title={tag.name} />)
-                    }
-                </footer>
-            }
+                {
+                    data.tags && 
+                    <footer>
+                        {
+                            data.tags.map(tag => <Tag key={tag.name} title={tag.name} />)
+                        }
+                    </footer>
+                }
         </Container>
     );
 }
