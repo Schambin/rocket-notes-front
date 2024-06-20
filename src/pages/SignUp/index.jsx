2d1f0133be956cb083e7c262e8ac9a2255af410a
Kauan <raidoka546@gmail.com>
2024-06-20 16:13:17 -0400

    SignUp page n styles

---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,44 @@
+import { Container, Form, Background } from "./styles";
+import { FaUser, FaEnvelope, FaLock } from "react-icons/fa6";
+
+
+import { Input } from "../../components/input"
+import { Button } from "../../components/button"
+
+export function SignUp () {
+    return (
+        <Container>
+            <Background />
+
+            <Form>
+                <h1>Rocket Notes</h1>
+                <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+                <h2>Crie sua conta</h2>
+
+                <Input 
+                    placeholder="Nome" 
+                    type="text" 
+                    icon={FaUser}>
+                </Input>
+
+                <Input
+                    placeholder="Email" 
+                    type="email" 
+                    icon={FaEnvelope }>
+                </Input>
+
+                <Input 
+                    placeholder="Senha" 
+                    type="password" 
+                    icon={FaLock}>
+                </Input>
+
+                <Button title="Cadastrar"/>
+
+                <a href="#"> Fazer Login </a>
+            </Form>
+
+        </Container>
+    )
+}
