ed300a068552a73a73b0c6fe39af19a8cff02e8b
Kauan <raidoka546@gmail.com>
2024-06-26 16:46:09 -0400

    application routes

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,4 +1,4 @@
-import {Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import { NewNote } from '../pages/NewNote';
 import { Home } from '../pages/Home';
@@ -9,9 +9,9 @@ export function AppRoutes() {
     return (
         <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/New' element={<NewNote />} />
-            <Route path='/Profile' element={<Profile />} />
-            <Route path='/Details/:id' element={<Details />} />
+            <Route path='/new' element={<NewNote />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/details/:id' element={<Details />} />
         </Routes>
     )
 }
